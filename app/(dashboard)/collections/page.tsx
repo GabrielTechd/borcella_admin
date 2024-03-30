7dfe6092ea61fa5500c7e63402d24813535d7f16
GabrielTechd <bielneres7@gmail.com>
2024-03-30 00:18:01 -0300

    envinado projeto

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -37,12 +37,12 @@ const Collections = () => {
     <div className="px-10 py-5">
       <div className="flex items-center justify-between">
         <p className="text-heading2-bold">Collections</p>
-        <Button className="bg-blue-1 text-white" onClick={() => router.push("/collections/new")}>
+        <Button className="bg-blue-900 text-white" onClick={() => router.push("/collections/new")}>
           <Plus className="h-4 w-4 mr-2" />
           Create Collection
         </Button>
       </div>
-      <Separator className="bg-grey-1 my-4" />
+      <Separator className="bg-gray-300 my-4" />
       <DataTable columns={columns} data={collections} searchKey="title" />
     </div>
   );
